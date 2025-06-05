b6f33b83e8dc9fe34756f69327a4cf38d7698a8f
palkin.kirill <kirilpalkin@yandex.ru>
2025-06-05 23:57:18 +0500

    removed ssr

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,18 +1,22 @@
 import ConfigProvider from "antd/es/config-provider"
 import ruRU from "antd/locale/ru_RU"
+import { StrictMode } from "react"
 import { RouterProvider } from "react-router"
 
 import { ThemeProvider } from "@/shared/theme"
 
 import { router } from "./router"
+
 import "@/shared/styles/main.css"
 
 export const App = () => {
   return (
-    <ConfigProvider locale={ruRU}>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </ConfigProvider>
+    <StrictMode>
+      <ConfigProvider locale={ruRU}>
+        <ThemeProvider>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </ConfigProvider>
+    </StrictMode>
   )
 }
